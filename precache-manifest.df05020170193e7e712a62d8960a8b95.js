159aebea080638b6abd928a7e336938b2e319649
Krasimir Trendafiloff <ktrendafiloff@gmail.com>
2021-07-25 18:21:02 +0300

    Updates

---
+++ precache-manifest.df05020170193e7e712a62d8960a8b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d180a86f12fe2d2fa1202f6ea340529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8968487d11923a372b0a",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "7d9c6d01a8bf90baad73",
+    "url": "/static/css/main.185c70d0.chunk.css"
+  },
+  {
+    "revision": "8968487d11923a372b0a",
+    "url": "/static/js/2.1b09185e.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.1b09185e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9c6d01a8bf90baad73",
+    "url": "/static/js/main.68fe4556.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
